9a62463abcd3f9790b8a7c72aba56b47dae0b6c6
天捺文 <amanathumiki@gmail.com>
2017-04-18 17:33:02 +0900

    乗算関数を追加しました

--- Hello_KAGA/Hello_KAGA/main.cpp
+++ Hello_KAGA/Hello_KAGA/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int Multi(int x,int y)
+{
+	return x*y;
+}
+
 int main()
 {
 	cout << "err KAGA" << endl;
+
+	cout << "100*200" << Multi(100, 200);
+
 	return 0;
 }
