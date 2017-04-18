9d7a5b326feb918555baae395e94a74eddb97344
中野拓海 <naka-taku9290@ezweb.ne.jp>
2017-04-18 17:34:39 +0900

    加算の関数を追加

--- Hello_KAGA/Hello_KAGA/main.cpp
+++ Hello_KAGA/Hello_KAGA/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int Add(int x, int y)
+{
+	return x + y;
+}
+
 int main()
 {
 	cout << "err KAGA" << endl;
+
+	cout <<"200 + 100 = "<< Add(200 , 100) << endl;
+
+	for (;;);
+
 	return 0;
 }
